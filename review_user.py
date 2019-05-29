08e33fa8ef7acae64c2b7c7736e821b6ec69dd0b
Julian Cahill <cahill.julian@gmail.com>
2019-05-30 04:02:49 +1200

    added documentation. generate text-form of index_names file. minor formatting fixes

--- review_user.py
+++ review_user.py
@@ -7,15 +7,26 @@ import sys, json, os, time, subprocess
 __version__ = rsaved.__version__
 
 def padded_to(string, length, pad=' '):
+	'''Appends a padding character to a string until it reaches a desired length.'''
 	if len(string) >= length: return string
 	return string + pad*(length - len(string))
 
+# ty https://stackoverflow.com/questions/870652/pythonic-way-to-split-comma-separated-numbers-into-pairs
+def n_wise(seq, n):
+	return zip(*([iter(seq)]*n))
+
 def write_item_to_stream(item, stream):
+	'''Writes a reddit object (post or comment) to the stream in a human-readable format.
+	
+	Args:
+		item: The reddit object, like that found in index.pickle.gz
+		stream: something that implementes a write(str) function
+	'''
 	data = item['data']
 	kind = item['kind']
 
 	if kind == 't3': # regular post
-		stream.write(f"{padded_to(str(data['score']), 8)} | {data['title']} | {data['domain']}\n")
+		stream.write(f"{padded_to(str(data['score']), 7)} | {data['title']} | {data['domain']}\n")
 		stream.write(f"to /r/{data['subreddit']} by {data['author']} on {time.ctime(data['created'])} with {data['num_comments']} comments.\n")
 		stream.write("\n")
 		
@@ -48,6 +59,13 @@ if __name__ == "__main__":
 		
 		for item in index:
 			write_item_to_stream(item, stream)
+	
+	with open(f'user/{username}/index_names.txt', 'w') as f:
+		f.write(f'Generated on {time.ctime()}\n\n')
+		index_names = rsaved.load_index_names(username)
+		for sublist in n_wise(index_names, 5):
+			f.write(', '.join(sublist))
+			f.write('\n')
 	
 	print('Done. Launching viewer...')
 	subprocess.call(['mousepad', f'user/{username}/index_review.txt'])
