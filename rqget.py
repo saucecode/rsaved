cee40701ee7d6e849e6e86fd3900647253735e70
Julian Cahill <cahill.julian@gmail.com>
2019-05-31 21:10:39 +1200

    version 0.44 - default behavior is now to not overwrite files

--- rqget.py
+++ rqget.py
@@ -1,4 +1,4 @@
-# rqget.py/0.43
+# rqget.py/0.44
 # this is a drop-in replacement for wget.
 # it takes two optional arguments, but it really works best if you specify them.
 
@@ -7,14 +7,17 @@
 
 # shoutout to https://stackoverflow.com/questions/16694907/download-large-file-in-python-with-requests
 
-import requests, sys
+import requests, sys, os
 
-__version__ = '0.42'
+__version__ = '0.44'
 
 print(f'rqget.py/{__version__} (This was a mistake! edition)')
 
 def print_usage():
-	print('Usage:', sys.argv[0], '-O [output] --proxy [requests valid proxy url] [URL]')
+	print('Usage:', sys.argv[0], '-O [output] --proxy [requests valid proxy url] --no-overwrite [URL]')
+	print('-O [output]         Specifies an output file path. The folder must exist.')
+	print('--proxy [proxy url] Goes directly into python requests')
+	print('--overwrite         Will overwrite files instead of exiting with code 0 without download.')
 
 if len(sys.argv) < 2:
 	print_usage()
@@ -23,6 +26,7 @@ if len(sys.argv) < 2:
 output = None
 proxy = None
 url = None
+overwrite = False
 argv = iter(sys.argv[1:])
 
 while True:
@@ -35,6 +39,8 @@ while True:
 		output = next(argv)
 	elif test == '--proxy':
 		proxy = next(argv)
+	elif test == '--overwrite':
+		overwrite = True
 	else:
 		url = test
 
@@ -60,5 +66,8 @@ def download_file(url, proxy, output):
 					f.write(chunk)
 	return local_filename
 
-download_file(url, proxy, output)
+if os.path.exists(output) and not overwrite:
+	print('File', output, 'already exists!')
+else:
+	download_file(url, proxy, output)
 sys.exit(0)
