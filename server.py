631c2fb5f95cea9a97da96db6c6a70944017c4a1
Julian Cahill <cahill.julian@gmail.com>
2019-06-02 03:00:41 +1200

    local server now displays imgur albums!

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
-import os, rsaved, time, mimetypes
+import os, rsaved, time, mimetypes, json
 
 from bottle import route, run, template, abort, static_file, request
+
+cached_indices = {}
 
 @route('/')
 def indexPage():
@@ -15,6 +17,7 @@ def userPage(username=None):
 	limit = int(request.query.limit or 100)
 	
 	index = rsaved.load_index(username)
+	cached_indices[username] = index
 	
 	names_only = [item['data']['name'] for item in index]
 	try:
@@ -30,20 +33,6 @@ def userPage(username=None):
 		getLibraryResourceMimetype=getLibraryResourceMimetype
 	)
 
-@route('/u/<username>/res/<domain>/<name>')
-def getResource(username, domain, name):
-	domain_folder = f'user/{username}/library/{domain}'
-	
-	if not os.path.exists(domain_folder):
-		return abort(404, 'Resource not found')
-	
-	files = [f for f in os.listdir(domain_folder) if f.startswith(name)]
-	
-	if len(files) == 1:
-		return static_file(files[0], root=domain_folder)
-		
-	return str(files)
-
 @route('/u/<username>/res/<domain>/thumbs/<name>')
 def getResourceThumb(username, domain, name):
 	thumbs_folder = f'user/{username}/library/{domain}/thumbs'
@@ -51,9 +40,43 @@ def getResourceThumb(username, domain, name):
 	if not os.path.exists(thumbs_folder):
 		return abort(404, 'Resource not found')
 	
-	file = next(f for f in os.listdir(thumbs_folder) if f.startswith(name))
+	try:
+		file = next(f for f in os.listdir(thumbs_folder) if f.startswith(name))
+	except StopIteration:
+		return abort(404)
 	
 	return static_file(file, root=thumbs_folder)
+
+@route('/u/<username>/res/<domain>/<name>')
+def getResource(username, domain, name):
+	domain_folder = f'user/{username}/library/{domain}'
+	
+	if not os.path.exists(domain_folder):
+		return abort(404, 'Resource not found')
+	
+	# index = cached_indices[username]
+	index = rsaved.load_index(username)
+	corresponding_item = next(i for i in index if i['data']['name'] == name)
+	
+	files = [f for f in os.listdir(domain_folder) if f.startswith(name)]
+	# files = []
+	
+	if len(files) == 1:
+		if not os.path.isdir(f'{domain_folder}/{files[0]}'):
+			return static_file(files[0], root=domain_folder)
+			
+		else:
+			if corresponding_item['rsaved']['download']['class'] == 'image_album':
+				albumid = corresponding_item['rsaved']['download']['albumid']
+				return '<br/>'.join([ f'<img src="/u/{username}/res/{domain}/{name}.{albumid}/{i}" />' for i in os.listdir(f'{domain_folder}/{name}.{albumid}')])
+		
+	return str(files) + '\n\n' + json.dumps(corresponding_item, indent=4)
+
+@route('/u/<username>/res/<domain>/<name>/<fname>')
+def getResourceFromFolder(username, domain, name, fname):
+	domain_folder = f'user/{username}/library/{domain}'
+	#return str([fname, f'{domain_folder}/{name}'])
+	return static_file(fname, root=f'{domain_folder}/{name}')
 
 @route('/res/<fname>')
 def getStaticResource(fname):
